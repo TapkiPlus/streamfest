af30010b1c48df0756667fbcb4e3859e8a725375
xxx <sdfds@dsds.tt>
2019-12-10 15:51:00 +0300

    Initial commit

---
+++ staticPages/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+
+   path('', views.index, name='index'),
+   # path('index.html', RedirectView.as_view(url='/', permanent=False), name='index1'),
+   # path('index.php', RedirectView.as_view(url='/', permanent=False), name='index2'),
+   # # path('posts/', views.allPosts, name='allposts'),
+   # # path('post/<slug>/', views.showPost, name='showpost'),
+   # path('about/', views.about, name='about'),
+   # path('contacts/', views.contacts, name='contacts'),
+   # path('services/', views.services, name='services'),
+   # path('service/<slug>/', views.service, name='service'),
+   # path('robots.txt', views.robots, name='robots'),
+
+
+
+]
