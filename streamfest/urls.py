6474e97df139753f37cda999bef17225b8d11618
ddnnss <ddnnss.i1@gmail.com>
2020-05-26 21:07:39 +0300

    new template

--- streamfest/urls.py
+++ streamfest/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('sitemap.xml', sitemap, {'sitemaps':sitemaps}),
     path('', include('staticPages.urls')),
-    path('speaker/', include('speaker.urls')),
+    path('star/', include('speaker.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
